97d864eb9baa509397cb9c75bb643135b47134cb
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-01-06 12:44:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app=express();
+
+const port = process.env.port || 4000;
+
+app.use(express.static('public'))
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.resolve(__dirname,'pages/index.html'));
+})
+app.get('/map', (req,res) =>{
+    res.sendFile(path.resolve(__dirname,'pages/map.html'));
+})
+app.listen(port, () =>{
+    console.log("Server Running at 4000");
+})
